7181e3e9be835718eb95a3cd4531d750bb1c6339
Faheetah <Faheetah@users.noreply.github.com>
2018-11-29 16:09:43 -0600

    Better logging of requests

--- webhook/server.py
+++ webhook/server.py
@@ -1,5 +1,6 @@
 import logging
 from flask import Flask, request
+from pprint import pformat
 
 import cli
 from trello import Trello
@@ -9,5 +10,10 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''}, methods=['POST', 'GET'])
 @app.route('/<path:path>', methods=['POST', 'GET'])
 def root(path):
-    app.logger.info('{} {} {}'.format(request.method, request.path, request.get_json()))
+    app.logger.debug(pformat(request.get_json()))
+
+    json = request.get_json()
+    message = '{} :: {} :: {} :: {}'.format(json['action']['type'], json['action']['id'], json['action'].get('data').get('board').get('name'), json['action'].get('data').get('card').get('name'))
+
+    app.logger.info(message)
     return ''
