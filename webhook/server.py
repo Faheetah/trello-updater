8024ae75e21fad8c8a1f038106a34664aec8ee5a
Faheetah <Faheetah@users.noreply.github.com>
2018-12-07 10:52:36 -0600

    Clean up webhook server

--- webhook/server.py
+++ webhook/server.py
@@ -2,9 +2,6 @@ import logging
 from flask import Flask, request
 from pprint import pformat
 
-import cli
-from trello import Trello
-
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''}, methods=['POST', 'GET'])
@@ -13,7 +10,7 @@ def root(path):
     app.logger.debug(pformat(request.get_json()))
 
     json = request.get_json()
-    message = '{} :: {} :: {} :: {}'.format(json['action']['type'], json['action']['id'], json['action'].get('data').get('board').get('name'), json['action'].get('data').get('card').get('name'))
+    message = u'{} :: {} :: {} :: {}'.format(json['type'], json['id'], json.get('data').get('board', {}).get('name'), json.get('data').get('card', {}).get('name'))
 
     app.logger.info(message)
     return ''
