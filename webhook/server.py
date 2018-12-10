2173d98b0328ed657cb5a9db7c9e10f6ae21fb3e
Faheetah <Faheetah@users.noreply.github.com>
2018-12-10 17:30:02 -0600

    Properly trigger trello callbacks from engine

--- webhook/server.py
+++ webhook/server.py
@@ -10,16 +10,17 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''}, methods=['POST', 'GET'])
 @app.route('/<path:path>', methods=['POST', 'GET'])
 def root(path):
-    app.logger.debug(pformat(request.get_json()))
+    json = request.get_json().get('action')
 
-    json = request.get_json()
     if json:
+        app.logger.debug(pformat(request.get_json()))
         message = u'{} :: {} :: {} :: {}'.format(json['type'], json['id'], json.get('data').get('board', {}).get('name'), json.get('data').get('card', {}).get('name'))
         app.logger.info(message)
 
     with open('trello.yml') as t:
         ruleset = t.read()
 
+    # @todo this needs to get refactored out to spawn multiple apps per trello instance
     e = Engine(ruleset, [Trello])
-    e.callback(json)
+    e.modules['trello'].callback(json)
     return ''
