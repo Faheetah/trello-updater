ea3688d7a49673bdba00ef8ad30efecac4b4d6d3
Faheetah <Faheetah@users.noreply.github.com>
2019-03-04 15:23:39 -0600

    Noop HEAD

--- modules/trello/server.py
+++ modules/trello/server.py
@@ -7,29 +7,11 @@ from engine.engine import Engine
 trello = Blueprint('trello', __name__)
 
 
-def init_webhook():
-    _, _, config = cli.parse()
-    trello = Trello(config['api_key'], config['api_token'], config['board'])
-    webhooks = trello.list_webhooks()
-
-    if not [wh for wh in webhooks if wh['callbackURL'] == config['webhook']]:
-        for i in range(5):
-            current_app.logger.info('creating webhook for {}'.format(config['webhook']))
-            try:
-                try:
-                    trello.delete_webhook(config['webhook'])
-                except:
-                    pass
-                trello.add_webhook(config['webhook'])
-                return
-            except Exception:
-                current_app.logger.info('retrying in {} seconds'.format(i))
-                time.sleep(i)
-        current_app.logger.warning('could not create webhook for {}'.format(config['webhook']))
-
-
 @trello.route('/', methods=['POST', 'GET'])
 def webhook():
+    if request.method == 'HEAD':
+        return ''
+
     json = request.get_json().get('action')
 
     if json:
