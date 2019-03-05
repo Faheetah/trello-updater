11af3beef1df9b3405e920b91f402022b53a2615
Faheetah <Faheetah@users.noreply.github.com>
2019-03-05 16:12:37 -0600

    Fix logging

--- cli/commands/server.py
+++ cli/commands/server.py
@@ -6,6 +6,23 @@ import cli
 from modules import modules
 from engine import Engine
 
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
 def main(config, *args):
     'start a webhook server for trello'
 
