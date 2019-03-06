f8dfce146531724f332bca8c4d7ab87c77eaeae6
Faheetah <Faheetah@users.noreply.github.com>
2019-03-05 23:04:39 -0600

    Refactor task

--- engine/task.py
+++ engine/task.py
@@ -4,18 +4,17 @@ class Task(object):
     def __init__(self, module, args):
         self.args = args
         self.module = module
-        self.name = args.pop('name', None)
+        self.name = args.iteritems().next()[1].pop('name', None)
     
     def run(self, conditionals, bindings=None):
         if bindings == None:
             bindings = conditionals
-        for task in self.args:
-            task_name = self.args[task].keys()[0]
+        for module_name, task in self.args:
             templated_tasks = {}
-            for k, v in self.args[task][task_name].iteritems():
+            for k, v in self.args[task][module_name].iteritems():
                 if isinstance(v, basestring):
                     templated_tasks[k] = Template(v).render(**bindings)
                 else:
                     templated_tasks[k] = v
             print("{0} :: {1}".format(task, templated_tasks))
-            return self.module.tasks[task_name](**templated_tasks)
+            return self.module.tasks[module_name](**templated_tasks)
