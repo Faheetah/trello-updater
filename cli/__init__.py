222097dc08e926a60ff474b09cb285b64ba3c747
Faheetah <Faheetah@users.noreply.github.com>
2019-03-06 00:16:25 -0600

    Don't hard code logging path

--- cli/__init__.py
+++ cli/__init__.py
@@ -34,10 +34,10 @@ def parse():
 
     args, extra_args = parser.parse_known_args()
 
-    if args.verbose:
-        logging.basicConfig(filename='flask.log',level=logging.DEBUG)
-    else:
-        logging.basicConfig(filename='flask.log',level=logging.INFO)
+    # if args.verbose:
+        # logging.basicConfig(filename='flask.log',level=logging.DEBUG)
+    # else:
+        # logging.basicConfig(filename='flask.log',level=logging.INFO)
 
     with open(args.config, 'r') as t:
         config = yaml.load(t)
