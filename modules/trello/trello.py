9a88c3d991adddab63bfb778c1d60f83e8e2e8d7
Faheetah <Faheetah@users.noreply.github.com>
2019-03-05 20:59:49 -0600

    Syntax error

--- modules/trello/trello.py
+++ modules/trello/trello.py
@@ -101,7 +101,7 @@ class Trello(object):
 
     def create_card(self, name, description, list_name):
         lists = self.list_lists()
-        l = in [l for l in lists if l['name'] == list_name]:
+        l = [l for l in lists if l['name'] == list_name]:
         if not l:
             l = self.create_list(list_name)
         return self.request('POST', '/boards/{}/cards'.format(self.board), params={'name': name, 'description': description, idList: l['id']})
