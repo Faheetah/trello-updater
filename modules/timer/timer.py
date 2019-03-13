5f2f700df1a68bb912cadeca06066d8f9496593e
Faheetah <Faheetah@users.noreply.github.com>
2019-03-13 11:23:24 -0500

    Get weekday

--- modules/timer/timer.py
+++ modules/timer/timer.py
@@ -1,10 +1,23 @@
 import time
+from datetime import datetime, timedelta
 
 from trigger import TimerTrigger
+
+# Order matters here, matches the datetime weekends
+DAYS = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday"
+]
 
 class Timer(object):
     def __init__(self, *args, **kwargs):
         self.tasks = {
+            'weekday': self.sleep,
             'sleep': self.sleep
         }
 
@@ -12,3 +25,11 @@ class Timer(object):
     
     def sleep(self, length):
         time.sleep(length)
+
+    def weekday(self, weekday, time="00:00:00"):
+        now = datetime.now()
+        delta = timedelta((7 + DAYS.index(weekday.lower()) - now.weekday()) % 7)
+        wd = now + delta
+        replace = datetime.strptime(time, "%H:%M:%S")
+        return wd.replace(hour=replace.hour, minute=replace.minute, second=replace.second).isoformat
+    
