4654fd2784897d1cf346b11f1a002e2c2868d5a7
Faheetah <Faheetah@users.noreply.github.com>
2019-05-24 14:45:14 -0500

    Nested configs

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import os
 import sys
 import yaml
 
@@ -34,15 +35,62 @@ def parse():
 
     args, extra_args = parser.parse_known_args()
 
+    config = parse_config_path(args.config)
+
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
-    with open(args.config, 'r') as t:
-        config = roll_up_keys(yaml.load(t))
 
     return args, extra_args, config
+
+def parse_config_path(path):
+    config = {}
+
+    if os.path.isfile(path):    
+        with open(path, 'r') as t:
+            config = yaml.load(t)
+
+    if os.path.isdir(path):
+        for job in os.listdir(path):
+            job_path = os.path.join(path, job)
+            job_name = os.path.splitext(job)[0]
+
+            if os.path.isfile(job_path):
+                if job_name in config:
+                    print('duplicate key {}'.format(job))
+                    sys.exit(1)
+                with open(job_path, 'r') as t:
+                    config[job_name] = yaml.load(t)
+            
+            # this could be refactored cleaner
+            if os.path.isdir(job_path):
+                config[job_name] = {}
+                for task in os.listdir(job_path):
+                    task_path = os.path.join(job_path, task)
+                    task_name = os.path.splitext(task)[0]
+
+                    if os.path.isfile(task_path):
+                        if task_name in config[job_name]:
+                            print('duplicate key {}'.format(job))
+                            sys.exit(1)
+                        with open(task_path, 'r') as t:
+                            config[job_name][task_name] = yaml.load(t)
+
+                    if os.path.isdir(task_path):
+                        config[job_name][task_name] = []
+                        for spec in os.listdir(task_path):
+                            spec_path = os.path.join(task_path, spec)
+                            spec_name = os.path.splitext(spec)[0]
+                            if os.path.isfile(spec_path):
+                                if spec_name in config[job_name][task_name]:
+                                    print('duplicate key {}'.format(job))
+                                    sys.exit(1)
+                                with open(spec_path, 'r') as t:
+                                    config[job_name][task_name].append(yaml.load(t))
+
+    return roll_up_keys(config)
 
 def roll_up_keys(yaml):
     if isinstance(yaml, list):
